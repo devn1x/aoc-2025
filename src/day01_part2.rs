9831e1573e16bffc8508c2c0e39055d12f483b36
devn1x <16466128+devn1x@users.noreply.github.com>
2025-12-02 22:55:28 +0100

    Rewrite solution for day 1 part 2

--- src/day01_part2.rs
+++ src/day01_part2.rs
@@ -1,94 +1,55 @@
 use std::{fs, path::Path};
 
+//const INPUT_PATH: &str = "input/01/example.txt";
 const INPUT_PATH: &str = "input/01/input.txt";
 const INITIAL_VALUE: i32 = 50;
 
-#[derive(Debug, Copy, Clone)]
-enum DialMoveDirection {
-    L,
-    R
-}
-
-#[derive(Debug)]
-struct DialMove {
-    direction: DialMoveDirection,
-    distance: i32
-}
-
-impl DialMove {
-    fn from_str(line: &str) -> Self {
-        let (direction, distance) = line.split_at(1);
-        let direction = match direction {
-            "L" => DialMoveDirection::L,
-            "R" => DialMoveDirection::R,
-            _ => panic!("Invalid direction")
-        };
-        DialMove { direction: direction, distance: distance.parse().unwrap() }
-    }
-}
-
-#[derive(Debug)]
-struct StateMachine {
-    value: i32
-}
-
-impl StateMachine {
-    fn apply(self: &mut Self, dial_move: &DialMove) -> i32 {
-        let previous = self.value;
-        let direction = match &dial_move.direction {
-            DialMoveDirection::L => -1,
-            DialMoveDirection::R => 1,
-        };
-        let total_distance = dial_move.distance * direction;
-        self.value += total_distance;
-
-        println!("Pre-normalize: {}", self.value);
-
-        //self.value = self.value % 100;
-        let mut total = 0;
-        while self.value < 0 || self.value >= 100 {
-            if self.value < 0 {
-                self.value += 100;
-            } else if self.value >= 100 {
-                self.value -= 100;
-            }
-        }
-
-        println!("{} -> {} = {} ({})", previous, self.value, self.value - previous, total_distance);
-        if previous + total_distance >= 100 {
-            total += (f64::from(previous + total_distance) / f64::from(100)).floor() as i32;
-        } else if previous + total_distance < 0 {
-            total += (f64::from(previous + total_distance) / f64::from(100)).floor() as i32 * -1;
-        }
-
-        return total;
-    }
-}
-
 pub fn main() {
     let path = Path::new(INPUT_PATH);
-    let mut state = StateMachine {
-        value: INITIAL_VALUE
-    };
-
     let content = fs::read_to_string(path).unwrap();
     
+    let mut value = INITIAL_VALUE;
     let mut total = 0;
     for line in content.lines() {
-        let dial_move = DialMove::from_str(line);
-        let zeroes = state.apply(&dial_move);
-        total += zeroes;
-        println!("Line: {:?} \t=> {:?} \t({:?})", dial_move, state.value, zeroes);
+        let direction = line.split_at(1).0;
+        let distance: i32 = line.split_at(1).1.parse().unwrap();
+        let next_value = match direction {
+            "L" => value - distance,
+            "R" => value + distance,
+            _ => panic!("Invalid direction found: {}", direction)
+        };
+
+        println!("Line: {}", line);
+        
+        match direction {
+            "L" => {
+                //println!("    {} -> {}", value, next_value);
+                if next_value <= 0 {
+                    let rounds = (value - 1).div_euclid(100) - (next_value - 1).div_euclid(100); // And then this fucking line
+                    if rounds > 0 {
+                        println!("    => {} times", rounds);
+                    }
+                    total += rounds;
+                }
+            },
+            "R" => {
+                //println!("    {} -> {}", value, next_value);
+                if next_value >= 100 {
+                    let rounds = next_value.div_euclid(100);
+                    if rounds > 0 {
+                        println!("    => {} times", rounds);
+                    }
+                    total += rounds;
+                }
+            },
+            _ => panic!("Invalid direction found: {}", direction)
+        }
+
+        value = next_value.rem_euclid(100);
+        //println!("    new: {}", value);
+
     }
 
-    println!("Final state: {:?}", state);
+    println!("Final value: {:?}", value);
     println!("Counted: {}", total);
-
-    println!("----- L953 -----");
-    let test = DialMove::from_str("L953");
-    let mut state = StateMachine { value: INITIAL_VALUE };
-    let zeroes = state.apply(&test);
-    println!("Test L953: {:?} => {:?} ({})", test, state.value, zeroes);
-    println!("----------------");
-    println!("{}", (f64::from(953) / f64::from(100)).floor());
 }
