5c07ddc51dc388871ce4fbaff0a263be0d9b9f76
devn1x <16466128+devn1x@users.noreply.github.com>
2025-12-04 21:11:32 +0100

    Add selector for puzzles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 mod day01_part1;
 mod day01_part2;
 mod day02_part1;
@@ -7,13 +9,48 @@ mod day03_part2;
 mod day04_part1;
 mod day04_part2;
 
+#[derive(Debug)]
+struct Puzzle {
+    day: u8,
+    part: u8,
+    main: fn()
+}
+
 fn main() {
-    //day01_part1::main();
-    //day01_part2::main();
-    //day02_part1::main();
-    //day02_part2::main();
-    //day03_part1::main();
-    //day03_part2::main();
-    //day04_part1::main();
-    day04_part2::main();
+    let puzzles: Vec<Puzzle> = vec![
+        Puzzle { day: 1, part: 1, main: day01_part1::main },
+        Puzzle { day: 1, part: 2, main: day01_part2::main },
+        Puzzle { day: 2, part: 1, main: day02_part1::main },
+        Puzzle { day: 2, part: 2, main: day02_part2::main },
+        Puzzle { day: 3, part: 1, main: day03_part1::main },
+        Puzzle { day: 3, part: 2, main: day03_part2::main },
+        Puzzle { day: 4, part: 1, main: day04_part1::main },
+        Puzzle { day: 4, part: 2, main: day04_part2::main }
+    ];
+
+    let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+    if args.len() >= 2 {
+        let argument = &args[1];
+        let (day, part) = argument.split_once(".").unwrap();
+        let day: u8 = day.parse().unwrap();
+        let part: u8 = part.parse().unwrap();
+
+        let puzzle: Vec<&Puzzle> = puzzles.iter().filter(|e| {
+            e.day == day && e.part == part
+        }).collect();
+        if puzzle.len() == 0 {
+            return;
+        }
+        let puzzle = puzzle[0];
+
+        println!("{:?}", puzzle);
+        (puzzle.main)();
+    } else {
+        let latest = puzzles.last();
+        if latest.is_some() {
+            let latest = latest.unwrap();
+            (latest.main)();
+        }
+    }
 }
