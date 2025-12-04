a1c9bbb063a67079b123e3e435f8488ab8ac3e60
devn1x <16466128+devn1x@users.noreply.github.com>
2025-12-04 19:22:41 +0100

    Add solutions for day 4

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod day02_part1;
 mod day02_part2;
 mod day03_part1;
 mod day03_part2;
+mod day04_part1;
+mod day04_part2;
 
 fn main() {
     //day01_part1::main();
@@ -11,5 +13,7 @@ fn main() {
     //day02_part1::main();
     //day02_part2::main();
     //day03_part1::main();
-    day03_part2::main();
+    //day03_part2::main();
+    //day04_part1::main();
+    day04_part2::main();
 }
