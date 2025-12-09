ce0401f255718a9fa2d30994a31be95a13d3b447
devn1x <16466128+devn1x@users.noreply.github.com>
2025-12-09 22:38:45 +0100

    Update progress

--- src/day08_part1.rs
+++ src/day08_part1.rs
@@ -1,16 +1,16 @@
-use std::{fs, num};
+use std::{collections::{HashMap, HashSet}, fs};
 
 const INPUT_PATH: &str = "input/08/example.txt";
 //const INPUT_PATH: &str = "input/08/input.txt";
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 struct Vector3 {
     x: i32,
     y: i32,
     z: i32
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct Circuit {
     junctions: Vec<Vector3>
 }
@@ -31,8 +31,8 @@ impl Vector3 {
 }
 
 impl Circuit {
-    fn append(self: &mut Self, other: &mut Self) {
-        self.junctions.append(&mut other.junctions);
+    fn extend(self: &mut Self, other: &Self) {
+        self.junctions.extend(other.junctions.clone());
     }
 }
 
@@ -43,19 +43,23 @@ pub fn main() {
     let mut total = 0;
     let mut circuits: Vec<Circuit> = vec![];
     for line in content.lines() {
-        println!("Line: {}", line);
+        //println!("Line: {}", line);
         let junction = Vector3::from_string(line);
         let mut circuit = Circuit { junctions: vec![] };
         circuit.junctions.push(junction);
         circuits.push(circuit);
     }
-    println!("{:#?}", circuits);
+    //println!("{:#?}", circuits);
 
     for iteration in 0..10 {
         let shortest_connection = shortest_distance(&circuits);
-        let circuit = circuit_by_junction(&shortest_connection.0);
-        let circuit2 = circuit_by_junction(&shortest_connection.1);
-        circuit.append(circuit2);
+        
+        let circuit_idx = circuit_index_by_junction(&circuits, &shortest_connection.0);
+        let circuit2_idx = circuit_index_by_junction(&circuits, &shortest_connection.1);
+
+        let circuit = &mut circuits[circuit_idx];
+        let circuit2 = circuits[circuit_index_by_junction(&circuits, &shortest_connection.1)].clone();
+        circuit.extend(&circuit2);
         //circuits.retain(|c| c != circuit2);
     }
 
@@ -64,9 +68,39 @@ pub fn main() {
 }
 
 fn shortest_distance(circuits: &Vec<Circuit>) -> (Vector3, Vector3) {
-    todo!()
+    let mut junctions: Vec<&Vector3> = vec![];
+    let mut distance_pairs: HashMap<(Vector3, Vector3), u64> = HashMap::new();
+    for circuit in circuits {
+        junctions.extend(&circuit.junctions);
+    }
+
+    for i in 0..junctions.len() {
+        for j in (i + 1)..junctions.len() {
+            let first = junctions[i].clone();
+            let second = junctions[j].clone();
+
+            let distance = first.distance(&second);
+            distance_pairs.insert((first, second), distance as u64);
+            //println!("Comparing {:?} to {:?} => {}", junctions[i], junctions[j], distance);
+        }
+    };
+
+    println!("Distance pairs: {:#?}", distance_pairs);
+
+    let mut sorted_pairs = distance_pairs.iter().collect::<Vec<_>>();
+    sorted_pairs.sort_by_cached_key(|element| {
+        element.1
+    });
+
+    println!("Sorted pairs: {:#?}", sorted_pairs);
+    sorted_pairs.first().unwrap().0.clone()
 }
 
-fn circuit_by_junction(junction: &Vector3) -> &'static mut Circuit {
-    todo!()
+fn circuit_index_by_junction<'a>(circuits: &Vec<Circuit>, junction: &'a Vector3) -> usize {
+    for (index, circuit) in circuits.iter().enumerate() {
+        if circuit.junctions.contains(junction) {
+            return index;
+        }
+    }
+    panic!("No circuit found for junction {:?}", junction);
 }
